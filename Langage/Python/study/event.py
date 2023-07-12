4465363250fac40eb71b37f663cc884c78634e40
happysh2005tony <130916343+happysh2005tony@users.noreply.github.com>
2023-07-12 23:45:04 +0900

    Add files via upload

---
+++ Langage/Python/study/event.py
@@ -0,0 +1,13 @@
+print("strat\n")
+for i in range(1, 9):
+    for j in range(1, 9):
+        multiply=i*j
+        print("%d x %d = %d", i, j, multiply)
+        #print(f"{i} x {j} = {multiply}")
+
+# print("★ 구구단을 출력합니다.\n")
+# for x in range(2, 10):
+#     print("------- [" + str(x) + "단] -------")
+#     for y in range(1, 10):
+#         print(x, "X", y, "=", x*y)
+# print("---------------------")
